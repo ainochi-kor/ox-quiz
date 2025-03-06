8cd769e0986f79baa1bb6d277d9df25bfc016e51
ainochi-kor <kthrkdals@gmail.com>
2025-03-06 14:26:14 +0900

    [console] update: navigation href 방식에서 react-router로 수정

--- apps/console/src/components/layout/Navigation.tsx
+++ apps/console/src/components/layout/Navigation.tsx
@@ -46,13 +46,23 @@ export function Navigation() {
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
               {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
-                </ListItem>
+                <li key={component.title}>
+                  <NavigationMenuLink asChild>
+                    <Link
+                      to={component.href}
+                      className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                      )}
+                    >
+                      <div className="text-sm font-medium leading-none">
+                        {component.title}
+                      </div>
+                      <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        {component.description}
+                      </p>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
               ))}
             </ul>
           </NavigationMenuContent>
@@ -61,29 +71,3 @@ export function Navigation() {
     </NavigationMenu>
   );
 }
-
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
-ListItem.displayName = "ListItem";
